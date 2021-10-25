0cb6be52339bc72800902b7491f1e63a855bd1c6
Nguyên Hùng <nguyenmanhhung110801@gmail.com>
2021-10-25 18:26:10 +0700

    Update

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 }
